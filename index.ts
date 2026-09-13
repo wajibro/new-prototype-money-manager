b03d82f1078f8cd935b32f3eb632050feb1cde92
Upi PC <wajibro@yahoo.com>
2026-09-14 00:29:16 +0700

    fix session

--- index.ts
+++ index.ts
@@ -45,11 +45,10 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-    httpOnly: true,
-    
-    secure: process.env.NODE_ENV === 'production', 
-    sameSite: 'lax',
-    maxAge: 1000 * 60 * 60 * 24
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', 
+        sameSite: 'lax',
+        maxAge: 1000 * 60 * 60 * 24
     }
 }));
 
